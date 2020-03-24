bffe86557bc4cd38551ce3e5fee5c85d080a1b5a
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-03-24 20:05:23 -0300

    Finish express and start styles

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,8 +12,33 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add the partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/beers', (req, res) => {
+    punkAPI.getBeers()
+    .then(response => {
+        console.log(response)
+        res.render('beers', { data: response });
+    });
+});
+
+app.get('/random-beer', (req, res) => {
+    punkAPI.getRandom()
+    .then(response => {
+        console.log(response)
+        res.render('random-beer', { data: response });
+    });
+});
+
+app.get('/beers/:id', (req, res) => {
+    punkAPI.getBeer(req.params.id)
+    .then(response => {
+        console.log(response)
+        res.render('beer', { data: response });
+    });
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
